3c73cad665d6d839f3dc05d06ee06d33281d95ec
GRND0 <zefiro.turchetta@gmail.com>
2022-04-11 16:52:40 +0200

    milestone 2 completata

---
+++ Milestone 2/script/script.js
@@ -0,0 +1,26 @@
+const rowWrapper = document.getElementById ("wrapper") ;
+
+
+// contatore 1-100
+
+for (let i = 1 ; i <= 100; i++ ) {
+   
+// identificatore fizz,buzz & fizzbuzz    
+    let multiplo = i; // undefined
+
+    if (i % 3 === 0) {
+        multiplo = "Fizz" ;
+       
+    } if (i % 5 === 0) {
+        multiplo = "Buzz" ;
+       
+    } if (i % 3 === 0 && i % 5 === 0 ) {
+        multiplo = "FizzBuzz" ;
+    }
+    rowWrapper.innerHTML +=  `<div class="box">${multiplo}</div>` 
+} 
+
+
+
+
+
